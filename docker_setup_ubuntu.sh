e4ca26d2468249aa0c0a657ad0086ae0a28bbc48
Chen Chen <chenchen.bme@gmail.com>
2018-12-11 07:07:04 +0800

    Update docker_setup_ubuntu.sh

--- docker_setup_ubuntu.sh
+++ docker_setup_ubuntu.sh
@@ -14,7 +14,7 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update   
-sudo apt-get install docker-ce
+sudo apt-get install docker-ce -y
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
