03ab424d60977756a293f953fd68a607059737e6
Chen Chen <chenchen.bme@gmail.com>
2018-12-11 07:06:42 +0800

    Update docker_setup_ubuntu.sh

--- docker_setup_ubuntu.sh
+++ docker_setup_ubuntu.sh
@@ -7,7 +7,7 @@ sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
-    software-properties-common
+    software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
