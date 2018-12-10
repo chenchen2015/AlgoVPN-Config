83226783965c8b5203c3669beb921f090583ab67
Chen Chen <chenchen.bme@gmail.com>
2018-12-11 07:06:11 +0800

    Update docker_setup_ubuntu.sh

--- docker_setup_ubuntu.sh
+++ docker_setup_ubuntu.sh
@@ -1,3 +1,27 @@
 # Setup docker and launch AlgoVPN image with the config file included in this repo
-sudo snap install docker
-sudo docker run --cap-drop ALL -it -v $PWD:/data trailofbits/algo:latest
+tmux
+
+# Setup docker for Ubuntu
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update   
+sudo apt-get install docker-ce
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+logout
+
+# start new
+tmux
+docker run hello-world
+
+#sudo docker run --cap-drop ALL -it -v $PWD:/data trailofbits/algo:latest
