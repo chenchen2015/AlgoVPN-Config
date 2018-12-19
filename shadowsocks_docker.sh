9775d871b1f6bcb75e2b7f00d8f496f8163b6e77
Chen Chen <chenchen.bme@gmail.com>
2018-12-19 08:36:47 +0800

    Update shadowsocks_docker.sh

--- shadowsocks_docker.sh
+++ shadowsocks_docker.sh
@@ -1,4 +1,6 @@
 sudo apt update
 sudo apt upgrade -yq
-sudo apt install python-pip -y
-pip install shadowsocks -U
+#sudo apt install python-pip -y
+#pip install shadowsocks -U
+# install docker
+sudo snap install docker
