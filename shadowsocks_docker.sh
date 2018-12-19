dd80bb7843d5b2f51de384a2ca0595a759652f4e
Chen Chen <chenchen.bme@gmail.com>
2018-12-19 10:12:00 +0800

    Update shadowsocks_docker.sh

--- shadowsocks_docker.sh
+++ shadowsocks_docker.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+# arguments: port password
 sudo apt update
 sudo apt upgrade -yq
 # install docker
 sudo snap install docker
-sudo docker run -dt --name vpn -p 6443:6443 mritd/shadowsocks -s "-s 0.0.0.0 -p 6443 -m aes-256-cfb -k [PWD_HERE] --fast-open"
+sudo docker run -dt --name vpn -p $1:$1 mritd/shadowsocks -s "-s 0.0.0.0 -p $1 -m aes-256-cfb -k $2 --fast-open"
